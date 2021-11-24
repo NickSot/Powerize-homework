a75a8c49a17ea908c93b793305c3c79c933e5adc
NickSot <nikola.s.sotirov@gmail.com>
2021-11-24 14:42:56 +0100

    initialize project, run skeleton tests

---
+++ test/MathStuffTestAuxiliary.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for auxiliary methods in {@code MathStuff}.
+ *
+<!--//# BEGIN TODO Name, student id, and date-->
+<p><b>Nikola, 1552686, 24/11/2021</b></p>
+<!--//# END TODO-->
+*/
+
+public class MathStuffTestAuxiliary {
+//    /**
+//     * Invokes (@code power(a, b)), and checks for expected value
+//     */
+//    private void checkPower(){}
+
+
+
+}
